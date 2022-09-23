fb8863ef793600e19d902f376688f6eba31f64e9
mmiladinova <mmiladinova@users.noreply.github.com>
2022-09-23 16:36:19 +0300

    Add files via upload

---
+++ FirstStepsInProgramming/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07.ProjectCreations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int projectCount = int.Parse(Console.ReadLine());
+
+            int totalHours = projectCount * 3;
+            Console.WriteLine($"The architect {name} will need {totalHours} hours to complete {projectCount} project/s.");
+        }
+    }
+}
