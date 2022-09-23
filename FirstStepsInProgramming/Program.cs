ca4efb9866f1012a3a11df9fc5226c08105a6d6a
mmiladinova <mmiladinova@users.noreply.github.com>
2022-09-23 16:38:31 +0300

    Add files via upload

---
+++ FirstStepsInProgramming/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace _08.PetShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogsPack = int.Parse(Console.ReadLine());
+            int catsPack = int.Parse(Console.ReadLine());
+            
+            double totalAmount = (catsPack * 4) + (dogsPack * 2.50);
+
+            Console.WriteLine($"{totalAmount} lv.");
+
+
+        }
+    }
+}
