26e90de2d7a7d6759f46434be4807a5a252087e1
mmiladinova <mmiladinova@users.noreply.github.com>
2022-09-24 23:08:30 +0300

    Add files via upload

---
+++ ConditionalStatements/L04.PasswordGuess1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace L04.PasswordGuess1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string enteredPass = Console.ReadLine();
+            string password = "s3cr3t!P@ssw0rd";
+
+            if (enteredPass == password)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
